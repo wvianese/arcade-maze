cbc28e53663ed1efeaa9e10ada23cb0605fc9942
Will <willv2003@icloud.com>
2026-02-10 17:37:06 -0600

    Make ohhhh extra long

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ function setYesMessage() {
     eyebrow.textContent = '';
   }
   if (headline) {
-    headline.innerHTML = 'OHHHHH<br>YEAH!<br><span class="aside">(Chick,</span><br><span class="aside">chicka-chicka)</span>';
+    headline.innerHTML = 'OHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH<br>YEAH!<br><span class="aside">(Chick,</span><br><span class="aside">chicka-chicka)</span>';
   }
   if (subhead) {
     subhead.textContent = '';
